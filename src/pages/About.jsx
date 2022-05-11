4af03a3306d090101a4684de9235a914b6fc005f
CTrotman-Dev <c.trotman92@gmail.com>
2022-05-11 20:26:49 +0100

    added about page content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,20 @@ const About = () => {
     return (
         <div className="">
             About
+            Delivering milk in Cardiff since 1983. Support doorstep deliveries and support your community.
+
+Milkman in Cardiff 
+About Us
+At Woodman’s, we supply and deliver a range of dairy products direct to your ho-
+me including, but not limited to, the following:
+
+All grades of milk,
+Potatoes,
+Fresh farm eggs,
+Cream,
+Fruit juices,
+Yogurts,
+Veg boxes.
         </div>
     );
 }
