2a6c86f876168c200f830c24e59fa0a4c83a693c
CTrotman-Dev <c.trotman92@gmail.com>
2022-05-11 20:21:04 +0100

    image on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     return (
         <div className="row">
             Home
-            
+            <img src="https://c7.alamy.com/zooms/3/31f70256734a41d58d7b4ae88526562c/m1089d.jpg"/>
         </div>
     );
 }
