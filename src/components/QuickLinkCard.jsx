979e14c6136dad893766ae7106eda73b9b6e43f3
CTrotman-Dev <c.trotman92@gmail.com>
2022-05-12 20:33:00 +0100

    converted a tags to Links in quicklinks

--- src/components/QuickLinkCard.jsx
+++ src/components/QuickLinkCard.jsx
@@ -1,16 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const QuickLinkCard = (props) => {
     return (
         <div className="col-md-4">
             <div className="quick-link ">
-
-                <a href={props.link}>
+                <Link to={props.link}>
                     <img src={props.imgSrc} />
                     <h1>
                         {props.text}
                     </h1>
-                </a>
+                </Link>
 
             </div>
         </div>
--- src/components/QuickLinks.jsx
+++ src/components/QuickLinks.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import QuickLinkCard from "./QuickLinkCard";
 
 const QuickLinks = () => {
