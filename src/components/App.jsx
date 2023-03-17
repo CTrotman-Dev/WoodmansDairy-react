7be5d5663a35f36b022065d62f5100bd6663f65d
CTrotman-Dev <@c.trotman92@gmail.com>
2023-03-17 13:49:03 +0000

    updated links

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
-import { StyledEngineProvider } from '@mui/material/styles';
+import { StyledEngineProvider } from "@mui/material/styles";
 
 import Header from "./Header";
 import Footer from "./Footer";
@@ -11,27 +11,25 @@ import Contact from "../pages/Contact";
 import Produce from "../pages/Produce";
 import News from "../pages/News";
 
-
-
 function App() {
-    return (
-        <StyledEngineProvider injectFirst>
-            <div>
-                <Header />
-                <div className="mainContent">
-                    <Routes>
-                        <Route exact path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/contact" element={<Contact />} />
-                        <Route path="/produce" element={<Produce />} />
-                        <Route path="/news" element={<News />} />
-                    </Routes>
-                </div>
-                <Footer />
-            </div>
-        </StyledEngineProvider>
-
-    );
+  return (
+    <StyledEngineProvider injectFirst>
+      <div>
+        <Header />
+        <div className="mainContent">
+          <Routes>
+            <Route element={<Home />} />
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/produce" element={<Produce />} />
+            <Route path="/news" element={<News />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </StyledEngineProvider>
+  );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,28 +3,67 @@ import { Link } from "react-router-dom";
 
 import { Navbar, Nav } from "react-bootstrap";
 
-
 // const isRegistered = false;
 // const isLoggedIn = false;
 function Header() {
-    return (
-        <Navbar collapseOnSelect className="navbar" variant="dark" expand="lg" sticky="top">
-        <Navbar.Brand href="/">
-        Woodman's Dairy
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse id="navbar-nav">
-          <Nav className="me-0">
-            <Nav.Link eventKey="0" as={Link} className="nav-item" href="/" to="/">Home</Nav.Link>
-            <Nav.Link eventKey="1" as={Link} className="nav-item" href="/produce" to="/produce">Produce</Nav.Link>
-            <Nav.Link eventKey="2" as={Link} className="nav-item" href="/news" to="/news">News</Nav.Link>
-            <Nav.Link eventKey="3" as={Link} className="nav-item" href="/about" to="/about">About</Nav.Link>
-            <Nav.Link eventKey="4" as={Link} className="nav-item" href="/contact" to="/contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-       
-    );
+  return (
+    <Navbar
+      collapseOnSelect
+      className="navbar"
+      variant="dark"
+      expand="lg"
+      sticky="top"
+    >
+      <Nav.Link eventKey="5" as={Link} className="nav-item" href="/" to="/">
+        <Navbar.Brand>Woodman's Dairy</Navbar.Brand>
+      </Nav.Link>
+
+      <Navbar.Toggle aria-controls="navbar-nav" />
+      <Navbar.Collapse id="navbar-nav">
+        <Nav className="me-0">
+          <Nav.Link eventKey="0" as={Link} className="nav-item" href="/" to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link
+            eventKey="1"
+            as={Link}
+            className="nav-item"
+            href="/produce"
+            to="/produce"
+          >
+            Produce
+          </Nav.Link>
+          <Nav.Link
+            eventKey="2"
+            as={Link}
+            className="nav-item"
+            href="/news"
+            to="/news"
+          >
+            News
+          </Nav.Link>
+          <Nav.Link
+            eventKey="3"
+            as={Link}
+            className="nav-item"
+            href="/about"
+            to="/about"
+          >
+            About
+          </Nav.Link>
+          <Nav.Link
+            eventKey="4"
+            as={Link}
+            className="nav-item"
+            href="/contact"
+            to="/contact"
+          >
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default Header;
